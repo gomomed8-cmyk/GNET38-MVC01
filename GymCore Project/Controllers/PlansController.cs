0c8195c884ff1f40f6c8b997622da56e4f388976
gomomed8-cmyk <gomomed8@gmail.com>
2026-05-22 20:01:21 +0300

    Add project files.

---
+++ GymCore Project/Controllers/PlansController.cs
@@ -0,0 +1,36 @@
+﻿using GymCore_Project.DbContexts;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GymCore_Project.Controllers
+{
+    public class PlansController : Controller
+    {
+
+        private readonly GymDbContext dbContext;
+
+        public PlansController()
+        {
+            dbContext = new GymDbContext();
+        }
+
+        //index action
+        public async Task<IActionResult> Index()
+        {
+            var Plans =  await dbContext.Plans.ToListAsync();
+            return View(Plans);
+        }
+        // details action
+        public async Task<IActionResult> Details(int Id)
+        {
+            var Plan=await dbContext.Plans.FindAsync(Id);
+            if(Plan is  null) 
+                return RedirectToAction(nameof(Index));
+            else
+                return View(Plan);
+
+        }
+    }
+}
---
+++ GymCore Project/DbContexts/GymDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GymCore_Project.Configrations;
+using GymCore_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymCore_Project.DbContexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=GymManagement;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+        public DbSet<Plan> Plans { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new PlanConfiguration());
+        }
+        
+    }
+}
